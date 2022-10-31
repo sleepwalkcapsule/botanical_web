4df69a9a37b29efe3255204d644d1b55f6a696bc
Mark Proskurin <growberry.mark@gmail.com>
2022-10-31 16:50:37 +0300

    Flask-Migrations и регистрация пользователей

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,7 +1,8 @@
 import re
 from webapp.db import db
-from flask import Flask #, render_template, flash, redirect, url_for
-from flask_login import LoginManager #, current_user, login_required
+from flask import Flask
+from flask_login import LoginManager
+from flask_migrate import Migrate
 from webapp.admin.views import blueprint as admin_blueprint
 from webapp.manuals.views import blueprint as manuals_blueprint
 from webapp.user.models import User
@@ -11,6 +12,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
     db.init_app(app)
+    migrate = Migrate(app, db)
 
     login_manager = LoginManager()
     login_manager.init_app(app)
